29ecf6fe4e64d9a38b86b149a9bb5215c0083d73
asdeoz <asdeoz@gmail.com>
2012-10-05 12:28:00 +0200

    Second commit
    Location start/stop fixed
    No updates
    Button working

--- src/com/alg/androidlocationonly/MainActivity.java
+++ src/com/alg/androidlocationonly/MainActivity.java
@@ -6,12 +6,14 @@ import android.app.Activity;
 import android.content.Context;
 //import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.*;
 
 public class MainActivity extends Activity {
 	
 	private TextView longitudeLabel;
 	private TextView latitudeLabel;
+	private TextView errorLabel;
 	
 	private LocationManager lm;
 	private LocationListener ll;
@@ -23,10 +25,11 @@ public class MainActivity extends Activity {
         
         longitudeLabel = (TextView)findViewById(R.id.longitude_label);
         latitudeLabel = (TextView)findViewById(R.id.latitude_label);
+        errorLabel = (TextView)findViewById(R.id.error_label);
         
         lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
         ll = new mylocationlistener();
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, ll);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
     }
 
     @Override
@@ -38,33 +41,31 @@ public class MainActivity extends Activity {
     @Override
     public void onResume()
     {
-    	if(lm!=null)
+    	super.onResume();
+    	if(lm==null)
     	{
     		lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-    		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, ll);
+    		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
     	}
-    	
-    	super.onResume();
     }
     
     @Override
     public void onPause()
     {
+    	super.onPause();
     	lm.removeUpdates(ll);
     	lm = null;
-    	
-    	super.onPause();
     }
     
     private class mylocationlistener implements LocationListener {
     	public void onLocationChanged(Location location) {
-    		if(location!=null) {
+    		//if(location!=null) {
     			//Log.d("LOCATION CHANGED", location.getLatitude()+"");
     			//Log.d("LOCATION CHANGED", location.getLongitude()+"");
-    			latitudeLabel.setText(getString(R.string.latitude_string) + " " + location.getLatitude());
-    			longitudeLabel.setText(getString(R.string.longitude_string) + " " + location.getLongitude());
+    			//latitudeLabel.setText(getString(R.string.latitude_string) + " " + location.getLatitude());
+    			//longitudeLabel.setText(getString(R.string.longitude_string) + " " + location.getLongitude());
     			//Toast.makeText(MainActivity.this, location.getLatitude() + " " + location.getLongitude(), Toast.LENGTH_LONG).show();
-    		}
+    		//}
     	}
     	public void onProviderDisabled(String provider) {
         }
@@ -72,5 +73,25 @@ public class MainActivity extends Activity {
         }
         public void onStatusChanged(String provider, int status, Bundle extras) {
         }
+    }
+    
+    public void lastLocationButton_Click(View button)
+    {
+    	Criteria criteria = new Criteria();
+    	criteria.setAccuracy(Criteria.ACCURACY_FINE);
+    	
+    	String provider = lm.getBestProvider(criteria, true);
+    	Location location = lm.getLastKnownLocation(provider);
+    	
+    	if (location!=null)
+    	{
+    		errorLabel.setText("");
+    		latitudeLabel.setText(getString(R.string.latitude_string) + " " + location.getLatitude());
+    		longitudeLabel.setText(getString(R.string.longitude_string) + " " + location.getLongitude());
+    	}
+    	else
+    	{
+    		errorLabel.setText(getString(R.string.error_gps_no_location_string));
+    	}
     }
 }
